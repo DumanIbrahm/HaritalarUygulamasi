b00edb87421869117395d5b90d8c020ab44fc592
Ibrahim Duman <dumanibrahm@gmail.com>
2023-03-11 18:13:46 +0300

    Kullanıcı konum ayarları yapıldı.

--- HaritalarUygulaması/ViewController.swift
+++ HaritalarUygulaması/ViewController.swift
@@ -7,15 +7,24 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController,MKMapViewDelegate {
+class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
 
     @IBOutlet weak var mapKit: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         mapKit.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
     }
     
 
